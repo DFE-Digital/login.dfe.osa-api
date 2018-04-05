be0cb12d6b0e3aa7c3b8fe4aab9bd04ba8d454f9
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2018-04-05 11:52:20 +0100

    Add validate credentials util

---
+++ src/app/authenticate/utils/validateCredentials.js
@@ -0,0 +1,31 @@
+const { createHash } = require('crypto');
+const oldSecureAccess = require('./../../../infrastructure/oldSecureAccess');
+
+const validateCredentials = (username, password, salt, osaUserName, osaPassword) => {
+  const hash = createHash('sha512');
+  hash.update(password + salt, 'utf8');
+  const hashed = hash.digest('hex');
+  return hashed === osaPassword && username.toLowerCase() === osaUserName.toLowerCase();
+};
+
+const validateOsaCredentials = async (username, password) => {
+  const user = await oldSecureAccess.getUserByUsername(username);
+
+  if (!user) {
+    return null;
+  }
+
+  const result = validateCredentials(username, password, user.salt, user.username, user.password);
+
+  if (result === false) {
+    return null;
+  }
+
+  return {
+    firstName: user.firstName,
+    lastName: user.lastName,
+    email: user.email,
+  };
+};
+
+module.exports = { validateOsaCredentials };
