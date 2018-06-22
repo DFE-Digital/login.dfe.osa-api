370eca4bedada16060aa833cda36597477be8d6f
Simon Bull <simon.bull.84@googlemail.com>
2018-06-22 16:00:08 +0100

    Add http to worker so IISNODE can monitor health

--- src/worker.js
+++ src/worker.js
@@ -1,6 +1,8 @@
 const schedule = require('node-schedule');
 const logger = require('./infrastructure/logger');
 const config = require('./infrastructure/config');
+const express = require('express');
+const healthCheck = require('login.dfe.healthcheck');
 
 const downloadAndRestoreOsaBackup = require('./app/osaRestore');
 const { startMonitoring, stopMonitoring } = require('./app/syncUsers');
@@ -21,4 +23,15 @@ process.once('SIGTERM', () => {
     logger.error(`Error stopping - ${e.message}`);
     process.exit(1);
   });
+});
+
+
+const port = process.env.PORT || 3000;
+const app = express();
+app.use('/healthcheck', healthCheck({ config }));
+app.get('/', (req, res) => {
+  res.send();
+});
+app.listen(port, () => {
+  logger.info(`Server listening on http://localhost:${port}`);
 });
