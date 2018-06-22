6ca816db339c18a32c365a2ab3df5257fa1acd9b
Simon Bull <simon.bull.84@googlemail.com>
2018-06-22 09:18:02 +0100

    Add AppInsights logging

--- src/infrastructure/logger/index.js
+++ src/infrastructure/logger/index.js
@@ -3,6 +3,8 @@
 const winston = require('winston');
 const config = require('./../config');
 const WinstonSequelizeTransport = require('login.dfe.audit.winston-sequelize-transport');
+const appInsights = require('applicationinsights');
+const AppInsightsTransport = require('login.dfe.winston-appinsights');
 
 const logLevel = (config && config.loggerSettings && config.loggerSettings.logLevel) ? config.loggerSettings.logLevel : 'info';
 
@@ -16,15 +18,31 @@ const loggerConfig = {
     debug: 5,
     silly: 6,
   },
-  colors: (config && config.loggerSettings && config.loggerSettings.colors) ? config.loggerSettings.colors : null,
+  colors: {
+    info: 'yellow',
+    ok: 'green',
+    error: 'red',
+    audit: 'magenta',
+  },
   transports: [],
 };
 
 loggerConfig.transports.push(new (winston.transports.Console)({level: logLevel, colorize: true}));
 
 const sequelizeTransport = WinstonSequelizeTransport(config);
+
 if (sequelizeTransport) {
   loggerConfig.transports.push(sequelizeTransport);
+}
+
+if (config.hostingEnvironment.applicationInsights) {
+  appInsights.setup(config.hostingEnvironment.applicationInsights).setAutoCollectConsole(false, false).start();
+  loggerConfig.transports.push(new AppInsightsTransport({
+    client: appInsights.defaultClient,
+    applicationName: config.loggerSettings.applicationName || 'OSA-API',
+    type: 'event',
+    treatErrorsAsExceptions: true,
+  }));
 }
 
 const logger = new (winston.Logger)(loggerConfig);
