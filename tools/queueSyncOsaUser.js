9e737e1287a332c08e92b1d47c3f046c147c3865
Simon Bull <simon.bull.84@googlemail.com>
2018-08-10 06:42:26 +0100

    Fix non-return of orgs api calls to fix user sync error

--- tools/queueSyncOsaUser.js
+++ tools/queueSyncOsaUser.js
@@ -4,7 +4,7 @@ const connectionString = 'redis://127.0.0.1:6379?db=11';
 const queue = kue.createQueue({
   redis: connectionString,
 });
-const queuedJob = queue.create('syncosauser', { osaUsername: 'fagufaze', userId: 'BD211C7B-28FF-462D-932C-E0CFEEF80649' })
+const queuedJob = queue.create('syncosauser', { osaUsername: 'fagufaze', userId: 'A92D1615-F520-4D0D-A106-9A9554A8D1EF' })
   .save((err) => {
     if (err) {
       console.error(err.message);
