2ca7c3d08e03261f32033e20362a8483573118bc
Rhys Bridges <bridgesr@uni.coventry.ac.uk>
2018-08-24 14:22:47 +0100

    Return safe user

--- src/app/users/api/getUserByEmail.js
+++ src/app/users/api/getUserByEmail.js
@@ -1,5 +1,6 @@
 const { getUserByUsername, getUserByEmail } = require('./../../../infrastructure/oldSecureAccess');
 const logger = require('./../../../infrastructure/logger');
+const { safeUser } = require('./../../utils/safeUser');
 
 const getSAUser = async (req, res) => {
   const id = req.params.id;
@@ -11,7 +12,7 @@ const getSAUser = async (req, res) => {
     if (!user) {
       return res.status(404).send();
     }
-    return res.status(200).send(user);
+    return res.status(200).send(safeUser(user));
   } catch (e) {
     logger.error(e);
     throw e;
